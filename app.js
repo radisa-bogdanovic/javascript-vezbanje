70e9850963e0aabba7b755632071e1e956ed3f4f
radisa.bogdanovic <radisa.bogdanovic@algometric.net>
2024-01-24 18:21:47 +0100

    24-1-javascript-vezbanje

---
+++ app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// console.log("Cao ja sam slobo");
+
+// window.console.log("hehe");
+
+var mojeIme = "Radisa";
+
+const broj = 3123; //ne moze da se menja tokom vremena
+let mojeIme3 = "Radisa"; //moze da se menja tokom vremena
+
+const mojeIme4 = "hehe";
+let mojeIme5;
+
+let mojeIMe7 = null;
+mojeIme5 = 434;
+
+// console.log(mojeIme5);
+
+mojeIme5 = "op't dodeljenjo";
+
+("string"); // string- sve sto se nalazi u znacima navodnika "" '' ``
+("d"); // char karakter jedan string
+32131.43; // brojevi
+undefined; // to je kada varijabla nema vrednost
+null; //
+// boolean; // true ili false
+// true === istina;
+// false === laz;
+let brojJeVeciOdTri = 5 < 3;
+
+const slobodan = {
+	ime: "Mali SLobo",
+	godine: 25,
+	ozenjen: false,
+};
+console.log(slobodan.godine);
